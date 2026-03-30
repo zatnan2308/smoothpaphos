d36794823e8e4503875ddbca8570c045cfc346bc
zatnan2308 <zatnan2308@gmail.com>
2026-03-30 10:02:30 +0300

    bump version to 2.1.7

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
-define( 'SMOOTH_VERSION', '2.1.6' );
+define( 'SMOOTH_VERSION', '2.1.7' );
 define( 'SMOOTH_DIR', get_template_directory() );
 define( 'SMOOTH_URI', get_template_directory_uri() );
 
